04e4ca1585d032947719cd64beb4750dd90d1328
Jeongyeon <jeongyeon865@gmail.com>
2022-02-10 16:05:11 +0900

    jyeon-image_sharing

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.project.databinding.ActivityMainBinding
 import com.example.project.MyApplication
+import com.example.project.model.ItemData
+import com.example.project.recycler.MyAdapter
+import com.example.project.util.myCheckPermission
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +25,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        myCheckPermission(this)
+        binding.addFab.setOnClickListener {
+            if(MyApplication.checkAuth()) {
+                startActivity(Intent(this, AddActivity::class.java))
+            } else {
+                Toast.makeText(this, "인증을 먼저 진행해 주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onStart() {
@@ -44,6 +58,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeRecyclerView(){
-
+        // 컬랙션을 모두 가져오기
+        MyApplication.db.collection("news")
+            .get()
+            .addOnSuccessListener { result ->
+                val itemList = mutableListOf<ItemData>()
+                for (document in result) {
+                    val item = document.toObject(ItemData::class.java)
+                    item.docId=document.id
+                    itemList.add(item)
+                }
+                binding.mainRecyclerView.layoutManager=LinearLayoutManager(this)
+                binding.mainRecyclerView.adapter= MyAdapter(this, itemList)
+            }
+            .addOnFailureListener { exception ->
+                Log.d("kkang", "Error getting documents: ", exception)
+                Toast.makeText(this, "서버로부터 데이터 획득에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/example/project/MyApplication.kt
+++ app/src/main/java/com/example/project/MyApplication.kt
@@ -3,28 +3,42 @@ package com.example.project
 import androidx.multidex.MultiDexApplication
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 
 class MyApplication: MultiDexApplication() {
+
     companion object {
         lateinit var auth: FirebaseAuth
         var email: String? = null
-        fun checkAuth(): Boolean {
+
+        fun checkAuth(): Boolean{
             val currentUser = auth.currentUser
             return currentUser?.let {
                 email = currentUser.email
-                if (currentUser.isEmailVerified) {
+                if(currentUser.isEmailVerified){
                     true
-                } else {
+                }else {
                     false
                 }
             } ?: let {
                 false
             }
         }
+
+        lateinit var db: FirebaseFirestore
+        lateinit var storage: FirebaseStorage
+
     }
+
     override fun onCreate() {
         super.onCreate()
-        auth = Firebase.auth
+        auth =  Firebase.auth
+
+        db=FirebaseFirestore.getInstance()
+        storage=Firebase.storage
+
     }
 }
