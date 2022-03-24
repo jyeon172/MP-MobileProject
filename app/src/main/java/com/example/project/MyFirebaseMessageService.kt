30fa759d7cc0f5b02a96ec981b206d3657897b06
jeongyeonpark <jeongyeon865@gmail.com>
2022-03-24 22:57:21 +0900

    jyeon-firebase(unsolved)

--- app/src/main/java/com/example/project/MyFirebaseMessageService.kt
+++ app/src/main/java/com/example/project/MyFirebaseMessageService.kt
@@ -4,7 +4,9 @@ import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+// 해결 못 해써..
 class MyFirebaseMessageService : FirebaseMessagingService() {
+
     override fun onNewToken(p0:String) {
         super.onNewToken(p0)
         Log.d("kkang", "fcm token..........$p0")
