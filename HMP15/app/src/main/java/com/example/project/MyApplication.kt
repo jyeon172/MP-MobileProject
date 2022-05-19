462b97bd7db7507d2a61d812fd79c7a13a51965a
jeonyo <im210312@naver.com>
2022-05-19 18:21:49 +0900

    알림 적용

---
+++ HMP15/app/src/main/java/com/example/project/MyApplication.kt
@@ -0,0 +1,46 @@
+package com.example.project
+
+import androidx.multidex.MultiDexApplication
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+
+class MyApplication: MultiDexApplication() {
+
+    companion object {
+        lateinit var auth: FirebaseAuth
+        lateinit var prefs:PreferenceUtil
+        var email: String? = null
+
+        fun checkAuth(): Boolean{
+            val currentUser = auth.currentUser
+            return currentUser?.let {
+                email = currentUser.email
+                if(currentUser.isEmailVerified){
+                    true
+                }else {
+                    false
+                }
+            } ?: let {
+                false
+            }
+        }
+
+        lateinit var db: FirebaseFirestore
+        lateinit var storage: FirebaseStorage
+
+    }
+
+    override fun onCreate() {
+        prefs = PreferenceUtil(applicationContext)
+        super.onCreate()
+        auth =  Firebase.auth
+
+        db=FirebaseFirestore.getInstance()
+        storage=Firebase.storage
+
+    }
+}
