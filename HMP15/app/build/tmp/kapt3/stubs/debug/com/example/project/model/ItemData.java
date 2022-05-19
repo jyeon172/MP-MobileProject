2f0cfcefe2e0b25270aa7fce0d4f677917c1e1d3
jeonyo <im210312@naver.com>
2022-05-19 22:32:46 +0900

    알림

---
+++ HMP15/app/build/tmp/kapt3/stubs/debug/com/example/project/model/ItemData.java
@@ -0,0 +1,99 @@
+package com.example.project.model;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u001a\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002R\u001c\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001c\u0010\t\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u0006\"\u0004\b\u000b\u0010\bR\u001c\u0010\f\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u0006\"\u0004\b\u000e\u0010\bR\u001c\u0010\u000f\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0010\u0010\u0006\"\u0004\b\u0011\u0010\bR\u001c\u0010\u0012\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0013\u0010\u0006\"\u0004\b\u0014\u0010\bR\u001c\u0010\u0015\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0016\u0010\u0006\"\u0004\b\u0017\u0010\bR\u001c\u0010\u0018\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0019\u0010\u0006\"\u0004\b\u001a\u0010\bR\u001c\u0010\u001b\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001c\u0010\u0006\"\u0004\b\u001d\u0010\b\u00a8\u0006\u001e"}, d2 = {"Lcom/example/project/model/ItemData;", "", "()V", "category", "", "getCategory", "()Ljava/lang/String;", "setCategory", "(Ljava/lang/String;)V", "content", "getContent", "setContent", "date", "getDate", "setDate", "docId", "getDocId", "setDocId", "email", "getEmail", "setEmail", "imageYN", "getImageYN", "setImageYN", "title", "getTitle", "setTitle", "token", "getToken", "setToken", "app_debug"})
+public final class ItemData {
+    @org.jetbrains.annotations.Nullable()
+    private java.lang.String docId;
+    @org.jetbrains.annotations.Nullable()
+    private java.lang.String email;
+    @org.jetbrains.annotations.Nullable()
+    private java.lang.String content;
+    @org.jetbrains.annotations.Nullable()
+    private java.lang.String date;
+    @org.jetbrains.annotations.Nullable()
+    private java.lang.String title;
+    @org.jetbrains.annotations.Nullable()
+    private java.lang.String category;
+    @org.jetbrains.annotations.Nullable()
+    private java.lang.String imageYN;
+    @org.jetbrains.annotations.Nullable()
+    private java.lang.String token;
+    
+    public ItemData() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getDocId() {
+        return null;
+    }
+    
+    public final void setDocId(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getEmail() {
+        return null;
+    }
+    
+    public final void setEmail(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getContent() {
+        return null;
+    }
+    
+    public final void setContent(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getDate() {
+        return null;
+    }
+    
+    public final void setDate(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getTitle() {
+        return null;
+    }
+    
+    public final void setTitle(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getCategory() {
+        return null;
+    }
+    
+    public final void setCategory(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getImageYN() {
+        return null;
+    }
+    
+    public final void setImageYN(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final java.lang.String getToken() {
+        return null;
+    }
+    
+    public final void setToken(@org.jetbrains.annotations.Nullable()
+    java.lang.String p0) {
+    }
+}
